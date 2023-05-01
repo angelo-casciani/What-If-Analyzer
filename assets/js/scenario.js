f01c27abc9f276d54b08a057fd999f28fcb06e36
AngeloC99 <angelocasciani@live.it>
2023-05-01 17:18:54 +0200

    Handling Scenarios Data

--- assets/js/scenario.js
+++ assets/js/scenario.js
@@ -4,6 +4,12 @@ function fillData() {
     var formattedDate = sessionStorage.getItem("endDate").replace(/T/g, ' ').replace(/-/g, '/').replace(/"/g, "");
     document.getElementById("date").innerHTML += formattedDate;
 
+    chooseScenarios();
+
+    
+}
+
+function chooseScenarios() {
     if(!localStorage.getItem('scenarios')) {
         var nums = new Set();
         while (nums.size < 3) {
@@ -14,36 +20,44 @@ function fillData() {
     }
     
     scenarios = JSON.parse(localStorage.getItem('scenarios'));
-
     url_request = [`http://localhost:8000/simulations/scenario${scenarios[0]}.json`, `http://localhost:8000/simulations/scenario${scenarios[1]}.json`, `http://localhost:8000/simulations/scenario${scenarios[2]}.json`];
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url_request[0], true);
-    xhr.onload = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        console.log(data)
-    }
-    };
-    xhr.send();
 
-    var xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", url_request[1], true);
-    xhr2.onload = function() {
-    if (xhr2.readyState === 4 && xhr2.status === 200) {
-        var data2 = JSON.parse(xhr2.responseText);
-        console.log(data2)
+    if(!localStorage.getItem('scenario1')) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url_request[0], true);
+        xhr.onload = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            localStorage.setItem('scenario1', JSON.stringify(data));
+        }
+        };
+        xhr.send();
+        
     }
-    };
-    xhr2.send();
 
-    var xhr3 = new XMLHttpRequest();
-    xhr3.open("GET", url_request[2], true);
-    xhr3.onload = function() {
-    if (xhr3.readyState === 4 && xhr3.status === 200) {
-        var data3 = JSON.parse(xhr3.responseText);
-        console.log(data3)
+    if(!localStorage.getItem('scenario2')) {
+        var xhr2 = new XMLHttpRequest();
+        xhr2.open("GET", url_request[1], true);
+        xhr2.onload = function() {
+        if (xhr2.readyState === 4 && xhr2.status === 200) {
+            var data2 = JSON.parse(xhr2.responseText);
+            localStorage.setItem('scenario2', JSON.stringify(data2));
+        }
+        };
+        xhr2.send();
+        
     }
-    };
-    xhr3.send();
-    
+
+    if(!localStorage.getItem('scenario3')) {
+        var xhr3 = new XMLHttpRequest();
+        xhr3.open("GET", url_request[2], true);
+        xhr3.onload = function() {
+        if (xhr3.readyState === 4 && xhr3.status === 200) {
+            var data3 = JSON.parse(xhr3.responseText);
+            localStorage.setItem('scenario3', JSON.stringify(data3));
+        }
+        };
+        xhr3.send();
+        
+    }
 }
