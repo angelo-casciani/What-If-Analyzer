b9c2dde78de57835eddaf410a5de7b068c44bee5
AngeloC99 <angelocasciani@live.it>
2023-05-04 18:48:37 +0200

    Draft of Comparison page

--- assets/js/scenario.js
+++ assets/js/scenario.js
@@ -1,5 +1,10 @@
 function fillData() {
-    chooseScenarios(setScenarios);
+    if (!localStorage.getItem('scenario1') && !localStorage.getItem('scenario2') && !localStorage.getItem('scenario3')) {
+        chooseScenarios(setScenarios);
+    }
+    else {
+        setScenarios();
+    }
 }
 
 function setScenarios() {
@@ -236,10 +241,6 @@ function insertTime(scenario1,scenario2,scenario3){
         .range([height, 0])
         .domain([0, 4000]);
     
-    // vertical grid lines
-    // const makeXLines = () => d3.axisBottom()
-    //   .scale(xScale)
-    
     const makeYLines = () => d3.axisLeft()
         .scale(yScale)
     
@@ -249,15 +250,6 @@ function insertTime(scenario1,scenario2,scenario3){
     
     chart.append('g')
         .call(d3.axisLeft(yScale));
-    
-    // vertical grid lines
-    // chart.append('g')
-    //   .attr('class', 'grid')
-    //   .attr('transform', `translate(0, ${height})`)
-    //   .call(makeXLines()
-    //     .tickSize(-height, 0, 0)
-    //     .tickFormat('')
-    //   )
     
     chart.append('g')
         .attr('class', 'grid')
@@ -524,4 +516,353 @@ function insertMould(scenario1,scenario2,scenario3) {
         .attr('y', 40)
         .attr('text-anchor', 'middle')
         .text('Required Mould Changes Comparison')
+}
+
+function showScenarioComparison() {
+    var scenario1 = JSON.parse(localStorage.getItem('scenario1'));
+    var scenario2 = JSON.parse(localStorage.getItem('scenario2'));
+    var scenario3 = JSON.parse(localStorage.getItem('scenario3'));
+
+    insertCostComparison(scenario1,scenario2,scenario3);
+    insertTimeComparison(scenario1,scenario2,scenario3);
+
+    document.getElementById("cost1").innerHTML += scenario1['totalCostScenario'].toFixed(2) + " \u20AC";
+    document.getElementById("time1").innerHTML += ((scenario1['totalProductionTimeScenario']/60)/60).toFixed(2) + " h";
+    document.getElementById("mould1").innerHTML += scenario1['totalMouldChangesScenario'];
+
+    document.getElementById("cost2").innerHTML += scenario2['totalCostScenario'].toFixed(2) + " \u20AC";
+    document.getElementById("time2").innerHTML += ((scenario2['totalProductionTimeScenario']/60)/60).toFixed(2) + " h";
+    document.getElementById("mould2").innerHTML += scenario2['totalMouldChangesScenario'];
+
+    document.getElementById("cost3").innerHTML += scenario3['totalCostScenario'].toFixed(2) + " \u20AC";
+    document.getElementById("time3").innerHTML += ((scenario3['totalProductionTimeScenario']/60)/60).toFixed(2) + " h";
+    document.getElementById("mould3").innerHTML += scenario3['totalMouldChangesScenario'];
+}
+
+function insertCostComparison(scenario1,scenario2,scenario3) {
+    const sample = [
+        {
+            scenario: 'Scenario 1',
+            value: scenario1['totalCostScenario'].toFixed(2),
+            color: '#000000'
+        },
+        {
+            scenario: 'Scenario 2',
+            value: scenario2['totalCostScenario'].toFixed(2),
+            color: '#000000'
+        },
+
+        {
+            scenario: 'Scenario 3',
+            value: scenario3['totalCostScenario'].toFixed(2),
+            color: '#000000'
+        }
+    ];
+    
+    const svg = d3.select('svg');
+    const svgContainer = d3.select('#container');
+    
+    const margin = 80;
+    const width = 500 - 2 * margin;
+    const height = 300 - 2 * margin;
+    
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sample.map((s) => s.scenario))
+        .padding(0.2)
+    
+    // To handle the scale of the values on the y-axis
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 20000]);
+    
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+    
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+    
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+    
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat('')
+        )
+    
+
+    const barGroups = chart.selectAll()
+        .data(sample)
+        .enter()
+        .append('g')
+     
+    barGroups
+        .append('rect')
+        .attr('class', 'barr')
+        .attr('x', (g) => xScale(g.scenario))
+        .attr('y', (g) => yScale(g.value))
+        .attr('height', (g) => height - yScale(g.value))
+        .attr('width', xScale.bandwidth())
+        .attr('fill', function(d) {
+            if (d.scenario.includes('1')) {
+              return '#83d3c9';
+            } 
+            else if (d.scenario.includes('2')){
+                return '#49d49d'
+            }
+            else {
+              return '#15b097';
+            }
+          })
+        .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+            .attr('opacity', 0)
+    
+            d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 0.6)
+            .attr('x', (a) => xScale(a.scenario) - 5)
+            .attr('width', xScale.bandwidth() + 10)
+    
+        const y = yScale(actual.value)
+    
+        line = chart.append('line')
+            .attr('id', 'limit')
+            .attr('x1', 0)
+            .attr('y1', y)
+            .attr('x2', width)
+            .attr('y2', y)
+    
+        barGroups.append('text')
+            .attr('class', 'divergence')
+            .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+            .attr('y', (a) => yScale(a.value) + 30)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .text((a, idx) => {
+            const divergence = (a.value - actual.value).toFixed(2)
+            
+            let text = ''
+            if (divergence > 0) text += '+'
+            text += `${divergence}\u20AC`
+    
+            return idx !== i ? text : '';
+            })
+    
+        })
+        .on('mouseleave', function () {
+        d3.selectAll('.value')
+            .attr('opacity', 1)
+    
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 1)
+            .attr('x', (a) => xScale(a.scenario))
+            .attr('width', xScale.bandwidth())
+    
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+        })
+    
+    barGroups 
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `${a.value}\u20AC`)
+    
+    svg
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 3.3)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Total Cost (\u20AC)')
+
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'middle')
+        .text('Scenarios')
+    
+    svg.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Total Cost Comparison')
+}
+
+function insertTimeComparison(scenario1,scenario2,scenario3) {
+    const sample = [
+        {
+            scenario: 'Scenario 1',
+            value: ((scenario1['totalProductionTimeScenario']/60)/60).toFixed(2),
+            color: '#000000'
+        },
+        {
+            scenario: 'Scenario 2',
+            value: ((scenario2['totalProductionTimeScenario']/60)/60).toFixed(2),
+            color: '#000000'
+        },
+
+        {
+            scenario: 'Scenario 3',
+            value: ((scenario3['totalProductionTimeScenario']/60)/60).toFixed(2),
+            color: '#000000'
+        }
+    ];
+
+    const svg1 = d3.select('#containerTime').append("svg");
+    
+    const margin = 80;
+    const width = 500 - 2 * margin;
+    const height = 300 - 2 * margin;
+    
+    const chart = svg1.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+    
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sample.map((s) => s.scenario))
+        .padding(0.2)
+    
+    // To handle the scale of the values on the y-axis
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 4000]);
+    
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+    
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+    
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+    
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat('')
+        )
+    
+    const barGroups = chart.selectAll()
+        .data(sample)
+        .enter()
+        .append('g')
+    
+        barGroups
+        .append('rect')
+        .attr('class', 'barr')
+        .attr('x', (g) => xScale(g.scenario))
+        .attr('y', (g) => yScale(g.value))
+        .attr('height', (g) => height - yScale(g.value))
+        .attr('width', xScale.bandwidth())
+        .attr('fill', function(d) {
+            if (d.scenario.includes('1')) {
+              return '#83d3c9';
+            } 
+            else if (d.scenario.includes('2')){
+                return '#49d49d'
+            }
+            else {
+              return '#15b097';
+            }
+          })
+        .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+            .attr('opacity', 0)
+    
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 0.6)
+            .attr('x', (a) => xScale(a.scenario) - 5)
+            .attr('width', xScale.bandwidth() + 10)
+    
+        const y = yScale(actual.value)
+    
+        line = chart.append('line')
+            .attr('id', 'limit')
+            .attr('x1', 0)
+            .attr('y1', y)
+            .attr('x2', width)
+            .attr('y2', y)
+    
+        barGroups.append('text')
+            .attr('class', 'divergence')
+            .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+            .attr('y', (a) => yScale(a.value) + 30)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .text((a, idx) => {
+            const divergence = (a.value - actual.value).toFixed(2)
+            
+            let text = ''
+            if (divergence > 0) text += '+'
+            text += `${divergence}h`
+    
+            return idx !== i ? text : '';
+            })
+    
+        })
+        .on('mouseleave', function () {
+        d3.selectAll('.value')
+            .attr('opacity', 1)
+    
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 1)
+            .attr('x', (a) => xScale(a.scenario))
+            .attr('width', xScale.bandwidth())
+    
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+        })
+    
+    barGroups 
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `${a.value}h`)
+    
+    svg1
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 3.3)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Total Time (h)')
+    
+    svg1.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'middle')
+        .text('Scenarios')
+    
+    svg1.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Total Time Comparison')
 }
