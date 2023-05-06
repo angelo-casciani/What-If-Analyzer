b2007c3284d7b4b395d4f527f639b7f4e8df2cc5
AngeloC99 <angelocasciani@live.it>
2023-05-06 10:58:57 +0200

    Updated Comparison Details and Login Check Pages

--- assets/js/scenario.js
+++ assets/js/scenario.js
@@ -50,7 +50,6 @@ function chooseScenarios(callback) {
     callback();
 }
 
-
 function insertCost(scenario1,scenario2,scenario3){
     const sample = [
         {
@@ -525,6 +524,7 @@ function showScenarioComparison() {
 
     insertCostComparison(scenario1,scenario2,scenario3);
     insertTimeComparison(scenario1,scenario2,scenario3);
+    insertMouldComparison(scenario1,scenario2,scenario3);
 
     document.getElementById("cost1").innerHTML += scenario1['totalCostScenario'].toFixed(2) + " \u20AC";
     document.getElementById("time1").innerHTML += ((scenario1['totalProductionTimeScenario']/60)/60).toFixed(2) + " h";
@@ -774,13 +774,13 @@ function insertTimeComparison(scenario1,scenario2,scenario3) {
         .attr('width', xScale.bandwidth())
         .attr('fill', function(d) {
             if (d.scenario.includes('1')) {
-              return '#83d3c9';
+                return '#83d3c9';
             } 
             else if (d.scenario.includes('2')){
-                return '#49d49d'
+                return '#49d49d';
             }
             else {
-              return '#15b097';
+                return '#15b097';
             }
           })
         .on('mouseenter', function (actual, i) {
@@ -864,5 +864,185 @@ function insertTimeComparison(scenario1,scenario2,scenario3) {
         .attr('x', width / 2 + margin)
         .attr('y', 40)
         .attr('text-anchor', 'middle')
-        .text('Total Time Comparison')
+        .text('Total Time Comparison');
+}
+
+
+function insertMouldComparison(scenario1,scenario2,scenario3) {
+    const sample = [
+        {
+        scenario: 'Scenario 1',
+        value: scenario1['totalMouldChangesScenario'],
+        color: '#000000'
+        },
+        {
+        scenario: 'Scenario 2',
+        value: scenario2['totalMouldChangesScenario'],
+        color: '#00a2ee'
+        },
+        {
+        scenario: 'Scenario 3',
+        value: scenario3['totalMouldChangesScenario'],
+        color: '#fbcb39'
+        }
+    ];
+
+    const svg2 = d3.select('#containerMould').append("svg");
+    
+    const margin = 80;
+    const width = 500 - 2 * margin;
+    const height = 300 - 2 * margin;
+    
+    const chart = svg2.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+    
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sample.map((s) => s.scenario))
+        .padding(0.2)
+    
+    // To handle the scale of the values on the y-axis
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 25]);
+    
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+    
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+    
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+    
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat('')
+        )
+    
+    const barGroups = chart.selectAll()
+        .data(sample)
+        .enter()
+        .append('g')
+    
+        barGroups
+        .append('rect')
+        .attr('class', 'barr')
+        .attr('x', (g) => xScale(g.scenario))
+        .attr('y', (g) => yScale(g.value))
+        .attr('height', (g) => height - yScale(g.value))
+        .attr('width', xScale.bandwidth())
+        .attr('fill', function(d) {
+            if (d.scenario.includes('1')) {
+              return '#83d3c9';
+            } 
+            else if (d.scenario.includes('2')){
+                return '#49d49d'
+            }
+            else {
+              return '#15b097';
+            }
+          })
+        .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+            .attr('opacity', 0)
+    
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 0.6)
+            .attr('x', (a) => xScale(a.scenario) - 5)
+            .attr('width', xScale.bandwidth() + 10)
+    
+        const y = yScale(actual.value)
+    
+        line = chart.append('line')
+            .attr('id', 'limit')
+            .attr('x1', 0)
+            .attr('y1', y)
+            .attr('x2', width)
+            .attr('y2', y)
+    
+        barGroups.append('text')
+            .attr('class', 'divergence')
+            .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+            .attr('y', (a) => yScale(a.value) + 30)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .text((a, idx) => {
+            const divergence = (a.value - actual.value).toFixed(2)
+            
+            let text = ''
+            if (divergence > 0) text += '+'
+            text += `${divergence}h`
+    
+            return idx !== i ? text : '';
+            })
+    
+        })
+        .on('mouseleave', function () {
+        d3.selectAll('.value')
+            .attr('opacity', 1)
+    
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 1)
+            .attr('x', (a) => xScale(a.scenario))
+            .attr('width', xScale.bandwidth())
+    
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+        })
+    
+    barGroups 
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.scenario) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `${a.value}`)
+    
+    svg2
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 3.3)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Mould Changes (#)')
+    
+    svg2.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'middle')
+        .text('Scenarios')
+    
+    svg2.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Required Mould Changes Comparison')
+}
+
+
+function goToScenario(scenarioId) {
+    switch (scenarioId) {
+        case 1:
+            window.location.href = "scenario1.html";
+            break;
+        case 2:
+            window.location.href = "scenario2.html";
+            break;
+        case 3:
+            window.location.href = "scenario3.html";
+            break;
+        default:
+          dayName = "Invalid Scenario Id";
+      }
 }
